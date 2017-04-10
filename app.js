9f8da361e73fcadcfc10d62b180e089e3b3e511b
Sushil L <sushil_samson@hotmail.com>
2017-04-10 10:53:16 +1000

    dotenv

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('dotenv').config()
 var index = require('./routes/index');
 var api = require('./routes/api');
 

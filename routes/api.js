a6f85017fdb8a1528075da7a785ec1a702eee22c
Sushil L <sushil_samson@hotmail.com>
2017-04-10 11:34:12 +1000

    send email through post request

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,53 @@ var express = require('express')
 var router = express.Router()
 var helper = require('sendgrid').mail
 
+router.post('/:action', function(req, res, next){
+
+  var action = req.params.action
+  if(action == 'send'){
+    //send an email
+      var from_email = new helper.Email('sushil_samson@hotmail.com')
+      var to_email = new helper.Email(req.body.recipient)
+      var subject = req.body.subject
+      var content = new helper.Content('text/html', req.body.content)
+      var mail = new helper.Mail(from_email, subject, to_email, content)
+
+      var sg = require('sendgrid')(process.env.SENDGRID_API_KEY)
+      var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+      })
+
+      sg.API(request, function(error, response) {
+        if(error){
+          res.json({
+            confirmation:'fail',
+            message:error
+          })
+          return
+        }
+
+        res.json({
+          confirmation:'success',
+          resonse: response
+        })
+      })
+
+    return
+  }
+
+  res.json({
+    confirmation:'fail',
+    message: 'invalid action'
+  })
+})
+
 
 router.get('/:action', function(req, res, next) {
   var action = req.params.action
   if(action == 'send'){
     //send an email
-    // res.json({
-    //   confirmation:'success',
-    //   message: action
-    // })
       var from_email = new helper.Email('sushil_samson@hotmail.com')
       var to_email = new helper.Email('slamy666@gmail.com')
       var subject = 'Test email'
